f6081c4270a52f9602f939b81b3fa736535cf277
Brian Scherner <brianscherner@gmail.com>
2023-12-18 14:16:19 -0800

    Add seed data to database.

--- CretaceousApi/Models/Animal.cs
+++ CretaceousApi/Models/Animal.cs
@@ -6,5 +6,6 @@ namespace CretaceousApi.Models
     public string Name { get; set; }
     public string Species { get; set; }
     public int Age { get; set; }
+    public string Gender { get; set; }
   }
 }
